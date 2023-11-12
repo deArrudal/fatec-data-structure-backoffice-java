ea78971d8b20c16f9f073dc4f363f46d97f9ddc3
AlgoEstranho13 <rafael2001bsantos@gmail.com>
2023-11-12 14:43:48 -0300

    Commit telinhas

--- src/view/TelaHome.java
+++ src/view/TelaHome.java
@@ -5,6 +5,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaHome extends JFrame {
 
@@ -31,12 +34,30 @@ public class TelaHome extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaHome() {
+		setTitle("Backoffice");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnCarregar = new JButton("Carregar");
+		btnCarregar.setBounds(321, 23, 89, 23);
+		contentPane.add(btnCarregar);
+		
+		
+		JButton btnConsulta = new JButton("Consulta");
+		btnConsulta.setBounds(321, 83, 89, 23);
+		contentPane.add(btnConsulta);
+		
+		JButton btnCadastro = new JButton("Cadastro");
+		btnCadastro.setBounds(321, 143, 89, 23);
+		contentPane.add(btnCadastro);
+		
+		JButton btnCompra = new JButton("Compra");
+		btnCompra.setBounds(321, 203, 89, 23);
+		contentPane.add(btnCompra);
 	}
-
 }
