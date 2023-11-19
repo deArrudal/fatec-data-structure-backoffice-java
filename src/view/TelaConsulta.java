125195a3992f322759b143b3531ae658745aa7b9
Bibis <severianobianca@hotmail.com>
2023-11-19 19:52:44 -0300

    commit

--- src/view/TelaConsulta.java
+++ src/view/TelaConsulta.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentListener;
 
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,12 +17,10 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.PopupMenuEvent;
 import javax.swing.event.PopupMenuListener;
 
-
 import controller.ManterCategoria;
 import controller.ManterClientePF;
 import controller.ManterClientePJ;
 import controller.ManterProduto;
-
 
 import javax.swing.JComboBox;
 import javax.swing.SwingConstants;
@@ -42,14 +39,11 @@ public class TelaConsulta extends JFrame {
 	private JTextField campoPesquisa;
 	private JComboBox<String> cbCategoria;
 
-	ManterClientePF manterClienteCPF = new ManterClientePF();
-	ManterClientePJ manterClientePJ = new ManterClientePJ();
-	ManterCategoria manterCategoria = new ManterCategoria();
-	ManterProduto manterProduto = new ManterProduto();
+	// ManterClientePF manterClienteCPF = new ManterClientePF();
+	// ManterClientePJ manterClientePJ = new ManterClientePJ();
+	// ManterCategoria manterCategoria = new ManterCategoria();
+	// ManterProduto manterProduto = new ManterProduto();
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -63,9 +57,6 @@ public class TelaConsulta extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public TelaConsulta() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -93,38 +84,50 @@ public class TelaConsulta extends JFrame {
 		contentPane.add(campoPesquisa);
 		campoPesquisa.setColumns(10);
 
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(32, 83, 363, 127);
+		contentPane.add(scrollPane);
+
+		JTextArea resultadoPesquisa = new JTextArea();
+		scrollPane.setViewportView(resultadoPesquisa);
+
 		// Evento clique botão pesquisar
 		JButton btnPesquisarProduto = new JButton("Pesquisar");
 		btnPesquisarProduto.setBounds(280, 52, 114, 21);
 		contentPane.add(btnPesquisarProduto);
 
-
 		ActionListener pesquisar = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				valorCampo = campoPesquisa.getText();
-				categoria = cbCategoria.getSelectedItem().toString();
-				// METODO DE PESQUISA
-				switch (categoria) {
-				case "CLIENTE CPF":
-					pesquisaRetorno = ManterClientePF.consultaClientePF(valorCampo);
-					break;
-				case "CLIENTE CNPJ":
-					pesquisaRetorno = ManterClientePJ.consultaClientePJ(valorCampo);
-					break;
+				if (valorCampo.isEmpty()) {
+					resultadoPesquisa.setText("Campo vazio!");
+				} else {
+					categoria = cbCategoria.getSelectedItem().toString();
+					// METODO DE PESQUISA
+					switch (categoria) {
+					case "CLIENTE CPF":
+						// pesquisaRetorno = ManterClientePF.consultaClientePF(valorCampo);
+						break;
+					case "CLIENTE CNPJ":
+						// pesquisaRetorno = ManterClientePJ.consultaClientePJ(valorCampo);
+						break;
 
-				case "PRODUTO":
-					pesquisaRetorno = ManterProduto.consultaProduto(valorCampo);
-					break;
+					case "PRODUTO":
+						// pesquisaRetorno = ManterProduto.consultaProduto(valorCampo);
+						break;
 
-				case "CATEGORIA":
-					pesquisaRetorno = ManterCategoria.consultaCategoria(valorCampo);
-					break;
+					case "CATEGORIA":
+						// pesquisaRetorno = ManterCategoria.consultaCategoria(valorCampo);
+						break;
 
+					}
+					resultadoPesquisa.setText(pesquisaRetorno);
 				}
 			}
+
 		};
 		btnPesquisarProduto.addActionListener(pesquisar);
-
 
 		JLabel lblInforme = new JLabel();
 		categoria = cbCategoria.getSelectedItem().toString();
@@ -146,16 +149,16 @@ public class TelaConsulta extends JFrame {
 		contentPane.add(lblInforme);
 
 		PopupMenuListener p = new PopupMenuListener() {
-			
+
 			@Override
 			public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
-				switch (cbCategoria.getSelectedItem().toString()){
+				switch (cbCategoria.getSelectedItem().toString()) {
 				case "CLIENTE CPF":
 					lblInforme.setText("Informe o CPF para buscá-lo:");
 					break;
@@ -169,35 +172,49 @@ public class TelaConsulta extends JFrame {
 					lblInforme.setText("Informe o NOME DA CATEGORIA para buscá-lo:");
 					break;
 				}
-				
+
 			}
-			
+
 			@Override
 			public void popupMenuCanceled(PopupMenuEvent e) {
 				// TODO Auto-generated method stub
-				
+
 			}
 		};
 		cbCategoria.addPopupMenuListener(p);
-		
 
-		JButton btnAtualizarProduto = new JButton("Atualizar");
-		btnAtualizarProduto.setBounds(22, 232, 114, 21);
-		btnAtualizarProduto.setBackground(new Color(128, 255, 128));
-		btnAtualizarProduto.setForeground(new Color(0, 0, 0));
-		contentPane.add(btnAtualizarProduto);
+		// Evento clique botão atualiza
+		
+		JButton btnAtualizar = new JButton("Atualizar");
+		btnAtualizar.setBounds(22, 232, 114, 21);
+		btnAtualizar.setBackground(new Color(128, 255, 128));
+		btnAtualizar.setForeground(new Color(0, 0, 0));
+		contentPane.add(btnAtualizar);
+		
+		ActionListener atualizar = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		};
+		btnAtualizar.addActionListener(atualizar);
 
 		// Evento clique botão Excluir
 		JButton btnExcluirProduto = new JButton("Excluir");
 		btnExcluirProduto.setBounds(146, 232, 114, 21);
 		btnExcluirProduto.setBackground(new Color(255, 0, 0));
 		contentPane.add(btnExcluirProduto);
-
 		ActionListener excluir = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 				categoria = cbCategoria.getSelectedItem().toString();
 				valorCampo = campoPesquisa.getText();
-
+				if (pesquisaRetorno == null) {
+					resultadoPesquisa.setText("Registro não localizado, impossível excluir!");
+				} else {
+					TelaRemoverRegistro t = new TelaRemoverRegistro(pesquisaRetorno, valorCampo, categoria);
+					t.setVisible(true);
+					setVisible(false);
+				}
 			}
 		};
 		btnExcluirProduto.addActionListener(excluir);
@@ -206,21 +223,7 @@ public class TelaConsulta extends JFrame {
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.setBounds(312, 232, 114, 21);
 		contentPane.add(btnVoltar);
-
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBounds(32, 83, 363, 127);
-		contentPane.add(scrollPane);
-		
-		JTextArea textArea = new JTextArea();
-		scrollPane.setViewportView(textArea);
-		
-		
-
-
 		ActionListener voltar = new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 				TelaHome t = new TelaHome();
 				t.setVisible(true);
@@ -228,7 +231,6 @@ public class TelaConsulta extends JFrame {
 			}
 		};
 		btnVoltar.addActionListener(voltar);
-
 
 	}
 }
--- src/view/TelaRemoverRegistro.java
+++ src/view/TelaRemoverRegistro.java
@@ -7,8 +7,15 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.ManterCategoria;
+import controller.ManterClientePF;
+import controller.ManterClientePJ;
+import controller.ManterProduto;
+
 import java.awt.Dimension;
 import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
@@ -21,26 +28,12 @@ public class TelaRemoverRegistro extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaRemoverRegistro frame = new TelaRemoverRegistro();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	//ManterClientePF manterClienteCPF = new ManterClientePF();
+	//ManterClientePJ manterClientePJ = new ManterClientePJ();
+	//ManterCategoria manterCategoria = new ManterCategoria();
+	//ManterProduto manterProduto = new ManterProduto();
 
-	/**
-	 * Create the frame.
-	 */
-	public TelaRemoverRegistro() {
+	public TelaRemoverRegistro(String conteudo, String valorCampo, String categoria) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -54,27 +47,7 @@ public class TelaRemoverRegistro extends JFrame {
 		lblRemover.setForeground(new Color(253, 66, 66));
 		lblRemover.setBounds(152, 10, 163, 13);
 		contentPane.add(lblRemover);
-		
-		JButton btnVoltar = new JButton("Voltar");
-		btnVoltar.setPreferredSize(new Dimension(100, 25));
-		btnVoltar.setBounds(88, 166, 100, 23);
-		contentPane.add(btnVoltar);
-		
-		ActionListener voltar = new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				TelaSelecaoCadastro.main(null);
-				setVisible(false);
-			}
-		};
-		
-		btnVoltar.addActionListener(voltar);
-		
-		JButton btnConfirmarExcluirCNPJ = new JButton("Prosseguir");
-		btnConfirmarExcluirCNPJ.setPreferredSize(new Dimension(100, 25));
-		btnConfirmarExcluirCNPJ.setBounds(211, 166, 104, 23);
-		contentPane.add(btnConfirmarExcluirCNPJ);
+
 		
 		JLabel lblVocEstPrestes = new JLabel("Você está prestes a excluir o seguinte registro:");
 		lblVocEstPrestes.setForeground(new Color(253, 66, 66));
@@ -89,5 +62,53 @@ public class TelaRemoverRegistro extends JFrame {
 		JLabel registroRemovido = new JLabel("");
 		registroRemovido.setBounds(53, 84, 45, 13);
 		contentPane.add(registroRemovido);
+		
+		registroRemovido.setText(conteudo);
+
+		
+		// Evento clique voltar
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setPreferredSize(new Dimension(100, 25));
+		btnVoltar.setBounds(88, 166, 100, 23);
+		contentPane.add(btnVoltar);
+		ActionListener voltar = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				TelaConsulta t = new TelaConsulta();
+				t.setVisible(true);
+				setVisible(false);
+			}
+		};
+		btnVoltar.addActionListener(voltar);
+		
+		JButton btnConfirmarExcluir = new JButton("Prosseguir");
+		btnConfirmarExcluir.setPreferredSize(new Dimension(100, 25));
+		btnConfirmarExcluir.setBounds(211, 166, 104, 23);
+		contentPane.add(btnConfirmarExcluir);
+		ActionListener excluir = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				switch (categoria) {
+				case "CLIENTE CPF":
+					//ManterClientePF.excluirClientePF(valorCampo);
+					break;
+				case "CLIENTE CNPJ":
+					//ManterClientePJ.excluirClientePJ(valorCampo);
+					break;
+
+				case "PRODUTO":
+					//ManterProduto.excluirProduto(valorCampo);
+					break;
+
+				case "CATEGORIA":
+					//ManterCategoria.excluirCategoria(valorCampo);
+					break;
+				}
+				JOptionPane.showMessageDialog(btnConfirmarExcluir, "Registro Excluido");
+				TelaConsulta t = new TelaConsulta();
+				t.setVisible(true);
+				setVisible(false);
+			}	
+			
+		};
+		btnConfirmarExcluir.addActionListener(excluir);
 	}
 }
